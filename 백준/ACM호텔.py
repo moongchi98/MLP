2f091ac1b3a29f92f56dd99a2e1a03b7f41c98dd
moongchi98 <kwaknh98@gmail.com>
2021-01-28 00:30:41 +0900

    0127

---
+++ 백준/ACM호텔.py
@@ -0,0 +1,20 @@
+T = int(input())
+case = [input().split(" ") for i in range(T)]
+# print(case)
+for trial_case in case:
+
+    result = ""
+    H = int(trial_case[0])
+    N = int(trial_case[2])
+    floor = N % H
+    if floor == 0:
+        floor = H
+        room_no = N // H
+    else:
+        room_no = (N//H)+1
+
+    if room_no < 10:
+        result += str(floor) + "0" + str(room_no)
+    else:
+        result += str(floor)+ str(room_no)
+    print(result)
