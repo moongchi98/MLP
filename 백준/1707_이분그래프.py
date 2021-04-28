3fa746a294cb70c2cfeb20a4703f17c3058a31c1
moongchi98 <kwaknh98@gmail.com>
2021-04-28 22:19:15 +0900

    0428

--- 백준/1707_이분그래프.py
+++ 백준/1707_이분그래프.py
@@ -1,16 +1,16 @@
 from sys import stdin
 from collections import deque
 def bfs(x):
-    q = deque()
+    q = deque([])
     q.append(x)
     visit[x] = 1#처음시작점
     while q:
         v = q.popleft()
-        for next in G[v]:
-            if visit[next] == 0:
-                visit[next] = -v
-                q.append(next)
-            elif visit[next] == visit[v]:
+        for p in G[v]:
+            if visit[p] == 0:
+                visit[p] = -visit[v]
+                q.append(p)
+            elif visit[p] == visit[v]:
                 return False
     return True
 
@@ -19,7 +19,7 @@ for tc in range(T):
     V,E = map(int,stdin.readline().split())
     G =[[] for _ in range(V+1)]
     visit=[0]*(V+1)
-    flag = 'YES'
+    flag = True
     for _ in range(E):
         u,v = map(int,stdin.readline().split())
         G[u].append(v)
@@ -27,9 +27,12 @@ for tc in range(T):
     for i in range(1,V+1):
         if visit[i] == 0:
             if not bfs(i):
-                flag = 'NO'
+                flag = False
                 break
-    print(flag)
+    if flag:
+        print('YES')
+    else:
+        print('NO')
 
 
 
--- 백준/2589_보물섬.py
+++ 백준/2589_보물섬.py
@@ -1,28 +1,33 @@
 from sys import stdin
 from collections import deque
 
+def bfs(r,c):
+    D_max = 0
+    dr = [-1,1,0,0]
+    dc = [0,0,-1,1]
+    D= [[0]*M for _ in range(N)]
+    visit =[[0]*M for _ in range(N)]
+    Q = deque([])
+    Q.append((r,c))
+    visit[r][c] = 1
+    while Q:
+        r,c = Q.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0<=nr<N and 0<=nc<M and visit[nr][nc] == 0 and arr[nr][nc] == 'L':
+                D[nr][nc] = D[r][c] + 1
+                visit[nr][nc] = 1
+                Q.append((nr,nc)) #이거때문에 후보의 양이 달라져서 시간차이 나는듯..
+                if D[nr][nc] > D_max:
+                    D_max = D[nr][nc]
+    return D_max
 N,M = map(int,stdin.readline().split())
 arr = [list(stdin.readline().strip()) for _ in range(N)]
-print(arr)
-Q = deque([])
-D_max = 0
-dr = [-1,1,0,0]
-dc = [0,0,-1,1]
+result = 0
 for r in range(N):
     for c in range(M):
-        D =[[0]*M for _ in range(N)]
-        visit = [[0]*M for _ in range(N)] #방문체크용
         if arr[r][c] == 'L':
-            Q.append((r,c))
-            while Q:
-                r,c = Q.popleft()
-                visit[r][c] = 1
-                for i in range(4):
-                    nr = r + dr[i]
-                    nc = c + dc[i]
-                    if 0<=nr<N and 0<=nc<M and visit[nr][nc] == 0 and arr[nr][nc] == 'L':
-                        D[nr][nc] = D[r][c] + 1
-                        Q.append((nr,nc))
-
-print(D)
+            result = max(result,bfs(r,c))
+print(result)
 
